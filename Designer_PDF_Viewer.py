02b207ffaf811b47d8cc687d9c76b58924266199
Ailing Liu <37415993+AilingLiu@users.noreply.github.com>
2020-04-07 11:13:32 +0200

    Create Designer_PDF_Viewer.py

---
+++ Designer_PDF_Viewer.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    
+    max_val = 0
+
+    for letter in word:
+        max_val = max(max_val, h[ord(letter)-97])
+    
+    return max_val*len(word)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
