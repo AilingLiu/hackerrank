c546f113b42aa27d25076f30e1d498a751a4cacb
Ailing Liu <37415993+AilingLiu@users.noreply.github.com>
2020-04-21 10:18:57 +0200

    Create equalizeArray.py

---
+++ equalizeArray.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the equalizeArray function below.
+def equalizeArray(arr):
+    n = len(arr)
+
+    unique = set(arr)
+
+    max_count = max(arr.count(i) for i in unique)
+
+    return n - max_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
