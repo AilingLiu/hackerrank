3f7be2d53b155dcd32562628d8be9c5b2f5d97d1
Ailing Liu <37415993+AilingLiu@users.noreply.github.com>
2020-04-27 11:27:26 +0200

    Create howManyGames.py

---
+++ howManyGames.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    cnt = 0
+    total = 0
+
+    for val in range(p, m, -d):
+        total += val
+    
+        if total >= s:
+            return cnt
+        else:
+            cnt += 1
+
+    remains = (s - total)
+    cnt += (s - total)//m
+    return cnt
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
