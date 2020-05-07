47dff3857aa5cece0a27eadc1ff686f38288e5b1
Ailing Liu <37415993+AilingLiu@users.noreply.github.com>
2020-05-07 11:28:36 +0200

    Update fairRations.py

--- fairRations.py
+++ fairRations.py
@@ -9,18 +9,16 @@ import sys
 # Complete the fairRations function below.
 def fairRations(B):
     n = len(B)
-    if n==2 and sum(B)%2:
+    breads = 0
+
+    for pos, val in enumerate(B):
+        if val%2 and pos <= (n-2):
+            breads += 2
+            B[pos+1] += 1
+    if B[-1]%2: #if the last value after operation becomes odd
         return 'NO'
     else:
-        breads = 0
-        for pos, val in enumerate(B):
-            if val%2 and pos <= (n-2):
-                breads += 2
-                B[pos+1] += 1
-        if B[-1]%2:
-            return 'NO'
-        else:
-            return breads
+        return breads
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
